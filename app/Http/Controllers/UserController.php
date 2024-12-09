64f349fd24f71aee9a6fb4c1502871642cf4cca1
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-12-09 23:09:34 +0300

    Error: I can't display profile picture on the browser

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\Rules\File;
 
@@ -16,7 +17,7 @@ class UserController extends Controller
     }
 
     public function store(Request $request) 
-    {   
+    {     Log::info('Test');
          $userAttributes = $request->validate([
             'name' => ['required', 'string', 'max:50',  'regex:/^[a-zA-Z\s]+$/' ],
             'username' => ['required', 'string', 'alpha_dash', 'min:3', 'max:20', 'unique:users,username',  'not_regex:/[@#!$%^&*]/'],
@@ -25,9 +26,19 @@ class UserController extends Controller
             'profile_picture' => ['nullable', File::types(['png', 'jpg', 'webp'])],
         ]);
 
-        if($request->hasFile('profile_picture')) {
-            $userAttributes['profile_picture'] = $request->file('profile_picture')->store('profile_pictures', 'public');
-        }
+        if ($request->hasFile('profile_picture')) {
+    $userAttributes['profile_picture'] = $request->file('profile_picture')->storeAs(
+        'profile_images', // Directory inside 'public'
+        uniqid() . '.' . $request->file('profile_picture')->extension(), // Unique file name
+        'public' // Disk
+    );
+}
+    // if($request->hasFile('profile_picture')) {
+    //     $profilePicturePath = $request->profile_picture->store('profile_pictures');
+    //     $userAttributes['profile_picture'] = $profilePicturePath;
+    //     Log::info($userAttributes['profile_picture']);
+    // }
+
 
         $userAttributes['password'] = Hash::make($userAttributes['password']);
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,15 @@
 <x-layout>
     <header>
-        header
+        {{ $user }}
+        {{ $isMine }}
+        <pre>{{ asset($user->profile_picture) }}</pre>
+
+        <img width="400px" height="400px" src="{{ asset($user->profile_picture) }}" alt="">
+        <img data-src= "{{ Storage::get('public/images/' . $user->profile_picture) }}" alt="Card image cap">
+
+
+
+
+
     </header>
 </x-layout>
